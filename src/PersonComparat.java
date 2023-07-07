0d2d597f6a9fe8005e0c7124f2f1af4119f222ed
Rafael Gadimov <cowboycore@yandex.ru>
2023-07-07 21:01:12 +0300

    Измененно на лямбда выражение

--- src/PersonComparat.java
+++ src/PersonComparat.java
@@ -10,25 +10,11 @@ public class PersonComparat implements Comparator<Person> {
 
     @Override
     public int compare(Person o1, Person o2) {
-        String[] o1SurnameWords = o1.getSurname().split(" ");
-        String[] o2SurnameWords = o2.getSurname().split(" ");
+        return Comparator.comparing((Person p) -> p.getSurname().split(" ").length < maxWordsInSurname)
+                .thenComparingInt((Person p) -> p.getSurname().split(" ").length)
+                .reversed()
+                .thenComparingInt(Person::getAge)
+                .compare(o1, o2);
 
-        int o1SurnameWordsCount = o1SurnameWords.length;
-        int o2SurnameWordsCount = o2SurnameWords.length;
-
-        if (o1SurnameWordsCount < maxWordsInSurname && o2SurnameWordsCount < o2SurnameWordsCount) {
-            return 0;
-        } else if (o1SurnameWordsCount < maxWordsInSurname) {
-            return 1;
-        } else if (o2SurnameWordsCount < maxWordsInSurname) {
-            return -1;
-        } else {
-            int surnameWordsComparison = Integer.compare(o2SurnameWordsCount, o1SurnameWordsCount);
-            if (surnameWordsComparison != 0) {
-                return surnameWordsComparison;
-            } else {
-                return Integer.compare(o2.getAge(), o1.getAge());
-            }
-        }
     }
 }
